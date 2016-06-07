15aa97157e1b982d173e644570acc51f366d92f1
pmlandwehr <pmlandwehr@gmail.com>
2016-06-07 13:58:43 -0400

    Fix spacing

--- R/MMPP.R
+++ R/MMPP.R
@@ -19,9 +19,9 @@ library(reshape2)
 ## #' @examples
 ## #' repmat(X, m, n)
 repmat <- function(X, m, n) {
-  mx <- dim(X)[1]
-  nx <- dim(X)[2]
-  return(matrix(t(matrix(X, mx, nx*n)), mx*m, nx*n, byrow=T))
+    mx <- dim(X)[1]
+    nx <- dim(X)[2]
+    return(matrix(t(matrix(X, mx, nx*n)), mx*m, nx*n, byrow=T))
 }
 
 #' sensorMMPP
